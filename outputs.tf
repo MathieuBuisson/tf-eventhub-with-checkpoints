0471c5cf1b6f05bc51c804539e64a551ec4f2c2a
MathieuBuisson <matthieu.buisson@gmail.com>
2019-07-05 17:07:40 +0100

    Upgrade root module

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,12 @@
 output "eventhub_producer_connection_string" {
-  value = "${module.producer-access-policy.connection_string}"
+  value = module.producer-access-policy.connection_string
 }
 
 output "eventhub_consumer_connection_string" {
-  value = "${module.consumer-access-policy.connection_string}"
+  value = module.consumer-access-policy.connection_string
 }
 
 output "storage_connection_string" {
-  value = "${module.storage.connection_string}"
+  value = module.storage.connection_string
 }
+
--- variables.tf
+++ variables.tf
@@ -1,38 +1,39 @@
 variable "resource_group_name" {
   description = "Name of the resource group where the resources will be deployed"
-  type        = "string"
+  type        = string
 }
 
 variable "location" {
   description = "Azure region where resources will be located"
-  type        = "string"
+  type        = string
 }
 
 variable "namespace_name" {
   description = "Name of the Event Hubs namespace"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "environment" {
   description = "The type of environment the deployment is for"
-  type        = "string"
+  type        = string
   default     = "Dev"
 }
 
 variable "eventhub_name" {
   description = "Name of Event Hub"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "consumergroup_names" {
   description = "List of consumer groups to deploy"
-  type        = "list"
+  type        = list(string)
   default     = ["analytics", "product"]
 }
 
 variable "storage_account_name" {
   description = "Name of the storage account"
-  type        = "string"
+  type        = string
 }
+

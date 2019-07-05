48f4a60d5a32340afed3da84dba3497fcf4eb554
MathieuBuisson <matthieu.buisson@gmail.com>
2019-07-05 17:38:36 +0100

    Upgrade module eventhub-consumergroup

--- modules/eventhub-consumergroup/variables.tf
+++ modules/eventhub-consumergroup/variables.tf
@@ -1,19 +1,20 @@
 variable "consumer_group_names" {
   description = "List of consumer groups to deploy"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group where the consumer group will be deployed"
-  type        = "string"
+  type        = string
 }
 
 variable "namespace_name" {
   description = "Name of the Event Hubs namespace where the consumer group belongs"
-  type        = "string"
+  type        = string
 }
 
 variable "eventhub_name" {
   description = "Name of Event Hub where the consumer group belongs"
-  type        = "string"
+  type        = string
 }
+

cb3fe79e950bb5ebb6b483c461327fcf715dc2de
MathieuBuisson <matthieu.buisson@gmail.com>
2019-07-05 16:44:44 +0100

    terraform 0.12checklist done

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   # Preventing automatic upgrades to new versions that may contain breaking changes.
-  # Any non-beta version >= 1.25.0 and < 2.0.0
-  version = "~>1.25"
+  # Any non-beta version >= 1.31.0 and < 2.0.0
+  version = "~>1.31"
 }
 
 terraform {
-  required_version = ">= 0.11.13"
+  required_version = ">= 0.11.14"
 }
